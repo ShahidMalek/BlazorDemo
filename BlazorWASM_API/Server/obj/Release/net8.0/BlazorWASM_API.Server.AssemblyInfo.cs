0660ea68077d8eba47d0c1228680cefcae29abbe
Shahid <malekshahid42003@gamil.com>
2025-03-03 16:13:27 +0530

    Fixed GitHub Pages base href and paths

--- BlazorWASM_API/Server/obj/Release/net8.0/BlazorWASM_API.Server.AssemblyInfo.cs
+++ BlazorWASM_API/Server/obj/Release/net8.0/BlazorWASM_API.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWASM_API.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f09d74898064e89b512c3fcacbe69d6ae92a621b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6704d525000129f103fc41f1d91b27285e0c43d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWASM_API.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWASM_API.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
